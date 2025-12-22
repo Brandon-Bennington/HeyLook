5a463db72370084866fd1196e0b415c8bf028113
Brandon <brandonwbennington@gmail.com>
2025-12-22 13:50:49 -0300

    Initial Commit

---
+++ HeyLook/HeyLookApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeyLookApp.swift
+//  HeyLook
+//
+//  Created by Brandon Bennington on 22/12/25.
+//
+
+import SwiftUI
+
+@main
+struct HeyLookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
